65434493d53331481b1c516859b902dff73af655
kusunoki_game <54877207+menber1@users.noreply.github.com>
2024-08-09 07:33:32 +0900

    Delete ffmpeg_converter.bat

--- ffmpeg_converter.bat
+++
@@ -1,2 +0,0 @@
-chcp 65001
-"./ffmpeg-7.0.1-essentials_build/bin/ffmpeg" -i "C:/Users/user1/Music/Aerosmith/Devil's Got A New Disguise- The Very Best Of Aerosmith/02 Love In An Elevator.mp3" -ac 1 "./temp/sounds/music/game/records/11.ogg"
